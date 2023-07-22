d7cfe8a985162c90eabb3efc4b08c765c8fe1a90
https://github.com/pranjalpathak26051998 <pranjalpathak010@gmail.com>
2023-07-23 00:05:07 +0530

    hide api key

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,6 +1,10 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
+import Dotenv from '../.env'
+
+let key = Dotenv.API_KEY;
 module.exports = {
-    key: "471e136e0487f5c968090e9da10340fa",
+    key: `${key}`,
     base: "https://api.openweathermap.org/data/2.5/",
   };
   
+  // 471e136e0487f5c968090e9da10340fa
